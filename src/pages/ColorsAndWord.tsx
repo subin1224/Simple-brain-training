d276b96e83ec2fb529a95ee0caad8d0b96cf1486
mmeat512 <wndus0958@naver.com>
2023-07-19 21:30:36 +0900

    constants 폴더 생성

--- src/pages/ColorsAndWord.tsx
+++ src/pages/ColorsAndWord.tsx
@@ -4,35 +4,6 @@ import { Card } from 'components/card/Card';
 import { CardContent } from 'components/card/CardContent';
 import { CardTitle } from 'components/card/CardTitle';
 
-const colors = [
-  'blue',
-  'green',
-  'yellow',
-  'gray',
-  'pink',
-  'black',
-  'white',
-  'red',
-  'purple',
-  'beige',
-  'brown',
-  'orange',
-];
-
-const words = [
-  '파랑',
-  '초록',
-  '노랑',
-  '회색',
-  '분홍',
-  '검정',
-  '흰색',
-  '빨강',
-  '보라',
-  '살구',
-  '갈색',
-  '주황',
-];
 export function ColorsAndWord() {
   return (
     <PageLayout>
