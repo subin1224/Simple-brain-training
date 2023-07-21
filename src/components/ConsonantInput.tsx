8b113b17299c049d34fe3a5998ef1c4dfa4da802
subinLee <salllena33@gmail.com>
2023-07-21 18:38:43 +0900

    [#15]FindAniamlName Page 작업 1차 commit

--- src/components/ConsonantInput.tsx
+++ src/components/ConsonantInput.tsx
@@ -1,35 +1,41 @@
 /** @jsxImportSource @emotion/react */
-import { useState } from "react";
+import { useState } from 'react';
 
-import { Txt } from "../components/Txt";
-import { Input } from "../components/Input";
+import { Txt } from '../components/Txt';
+import { Input } from '../components/Input';
+import { QuestionFindAnimalName } from '../pages/FindAnimalName';
 
-import { GoArrowRight } from "react-icons/go";
+import { GoArrowRight } from 'react-icons/go';
 
 interface Props {
-  consonant: string;
+  question: QuestionFindAnimalName;
+  checkAnswer: (item: QuestionFindAnimalName, idx: number) => void;
 }
 
 export function ConsonantInput(props: Props) {
-  const { consonant } = props;
+  const { question, checkAnswer } = props;
+  const [inputValue, setInputValue] = useState('');
 
-  const [inputValue, setInputValue] = useState("");
+  /**
+   * checkAnswer ? idx ?
+   * checkAnswer --- start
+   */
 
   return (
     <div
       css={{
-        display: "grid",
-        gridTemplateColumns: "1fr 0.3fr 1fr",
-        height: "80px",
-        margin: "0 5px",
-        alignItems: "center",
+        display: 'grid',
+        gridTemplateColumns: '1fr 0.3fr 1fr',
+        height: '80px',
+        margin: '0 5px',
+        alignItems: 'center',
       }}
     >
-      <Txt typography="h3">{consonant}</Txt>
-      <GoArrowRight css={{ fontSize: "40px" }} />
+      <Txt typography='h3'>{question.consonant}</Txt>
+      <GoArrowRight css={{ fontSize: '40px' }} />
       <Input
         value={inputValue}
-        length={consonant.length}
+        length={question.consonant.length}
         onChange={setInputValue}
       />
     </div>
--- src/pages/FindAnimalName.tsx
+++ src/pages/FindAnimalName.tsx
@@ -1,45 +1,75 @@
-import { PageLayout } from "../components/PageLayout";
-import { Txt } from "../components/Txt";
-import { Card } from "../components/card/Card";
-import { CardContent } from "../components/card/CardContent";
-import { CardTitle } from "../components/card/CardTitle";
-import { ConsonantInput } from "components/ConsonantInput";
+import { CardFooter } from 'components/card/CardFooter';
+import { PageLayout } from '../components/PageLayout';
+import { Txt } from '../components/Txt';
+import { Card } from '../components/card/Card';
+import { CardContent } from '../components/card/CardContent';
+import { CardTitle } from '../components/card/CardTitle';
+import { ConsonantInput } from 'components/ConsonantInput';
+import { Button } from 'components/Button';
+import { extractConsonant } from 'utils/commonUtils';
 
 export function FindAnimalName() {
+  const answerFindAnimalName = [
+    '기린',
+    '코끼리',
+    '얼룩말',
+    '도롱뇽',
+    '하마',
+    '청설모',
+    '까마귀',
+    '멧돼지',
+    '다람쥐',
+    '도마뱀',
+    '독수리',
+    '참새',
+  ];
+  const questionFindAniamlName: QuestionFindAnimalName[] =
+    answerFindAnimalName.map((words) => {
+      return {
+        consonant: extractConsonant(words),
+        answer: words,
+        isAnswered: false,
+      };
+    });
+
+  const checkAnswer = (item: QuestionFindAnimalName, idx: number) => {
+    questionFindAniamlName[idx] = { ...item };
+  };
+
   return (
     <PageLayout>
       <Card>
         <CardTitle>
-          <Txt typography="h3">동물 이름 맞히기</Txt>
+          <Txt typography='h3'>동물 이름 맞히기</Txt>
           <br /> {/* 추후 수정 예정 */}
-          <Txt typography="h5">
+          <Txt typography='h5'>
             다음 제시된 자음을 보고 동물 이름을 맞혀보세요.
           </Txt>
         </CardTitle>
         <CardContent
           css={{
-            display: "grid",
-            gridTemplateColumns: "1fr 1fr",
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr',
           }}
         >
-          <ConsonantInput consonant="ㄱㄹ" />
-          <ConsonantInput consonant="ㅋㄲㄹ" />
-          <ConsonantInput consonant="ㅇㄹㅁ" />
-          <ConsonantInput consonant="ㄷㄹㄴ" />
-          <ConsonantInput consonant="ㅎㅁ" />
-          <ConsonantInput consonant="ㅊㅅㅁ" />
+          {questionFindAniamlName.map((item, idx) => (
+            <ConsonantInput
+              key={`${item.answer}` + idx}
+              question={item}
+              checkAnswer={checkAnswer}
+            />
+          ))}
         </CardContent>
+        <CardFooter>
+          <Button>Next</Button>
+        </CardFooter>
       </Card>
     </PageLayout>
   );
 }
 
-/*
-<ConsonantInput consonant="ㄲㅁㄱ" />
-          <ConsonantInput consonant="ㅁㄷㅈ" />
-          <ConsonantInput consonant="ㄷㄹㅈ" />
-          <ConsonantInput consonant="ㄷㅁㅂ" />
-          <ConsonantInput consonant="ㄷㅅㄹ" />
-          <ConsonantInput consonant="ㅊㅅ" />
-
-*/
+export interface QuestionFindAnimalName {
+  consonant: string;
+  answer: string;
+  isAnswered: boolean;
+}
