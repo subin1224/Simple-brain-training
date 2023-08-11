0ab6af67a9b133220aba90914e31d856df97de16
subinLee <salllena33@gmail.com>
2023-08-11 18:47:22 +0900

    [#31] Input Component variant add

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -10,13 +10,14 @@ import {
 interface Props
   extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
   value: string;
-  length: number;
+  length?: number;
+  variant?: 'standard' | 'filled' | 'outlined';
   onChange: (value: string) => void;
 }
 
 export const Input = forwardRef(
   (props: Props, ref: ForwardedRef<HTMLInputElement>) => {
-    const { value, length, onChange, ...rest } = props;
+    const { value, length, variant = 'standard', onChange, ...rest } = props;
 
     const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
       const newValue = e.target.value.slice(0, length);
@@ -28,12 +29,12 @@ export const Input = forwardRef(
         css={{
           width: '100%',
           padding: '0 18px',
-          border: 'none',
           color: 'dimgrey',
           fontSize: '40px',
           '&:focus': {
             outline: 'none',
           },
+          ...TYPE_VARIANTS[variant],
         }}
         type='text'
         value={value}
@@ -44,3 +45,25 @@ export const Input = forwardRef(
     );
   }
 );
+
+const TYPE_VARIANTS = {
+  standard: {
+    border: 'none',
+    outline: 'none',
+  },
+  filled: {
+    backgroundColor: '#e9e9e9',
+    border: 'none',
+    borderRadius: '8px',
+    transition: 'background .2s ease, color .1s ease, box-shadow .2s ease',
+    boxShadow: 'none',
+  },
+  outlined: {
+    padding: '10px',
+    outline: 'none',
+    border: 'none',
+    borderRadius: '8px',
+    transition: `background .2s ease,color .1s ease, box-shadow .2s ease`,
+    boxShadow: `inset 0 0 0 4px #e9e9e9`,
+  },
+};
